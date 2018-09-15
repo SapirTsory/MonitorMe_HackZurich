a659e84bc0e36d5acb7ab12ba63cd41f01d8ceef
daria <daria.ackerman@chorus.ai>
2018-09-15 05:45:43 +0300

    GET request working

--- app/src/main/java/hack/myzurich/MainActivity.java
+++ app/src/main/java/hack/myzurich/MainActivity.java
@@ -4,8 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +28,46 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("daria linor sapir hahaha");
+                final TextView mTextView = (TextView) findViewById(R.id.text);
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url ="https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec?id=1x5Ej14fWb2-4vXHG3rdboMSxybAUUTlAkvcfvQlPPOI&sheet=Sheet1";
+
+                //TODO --- get (and then post) --- to public google sheet
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                System.out.println("response:" + response.substring(0,500));
+                            }
+                        },
+
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                System.out.println("That didn't work!!!");
+                            }
+                        });
+
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+                System.out.println("done sending request");
+
+
             }
         });
     }
+
+
+
 
 }
 
