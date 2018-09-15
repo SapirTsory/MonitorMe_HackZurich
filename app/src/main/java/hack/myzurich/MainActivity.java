8b3ec6c8ca1c2efe0fb9abe0de4d1cf1c9002a2e
GILAD TSORY <giladtsory@h-MacBook-Air-sl-GILAD.local>
2018-09-15 21:02:47 +0200

    new checkBox

--- app/src/main/java/hack/myzurich/MainActivity.java
+++ app/src/main/java/hack/myzurich/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,15 +16,14 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    String url ="https://script.google.com/macros/s/AKfycby_J-YAi-S4DdH2WSwMOB3SjOzIqWCYqRC1fEg2ddkNJroOXiF6/exec?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         postRequest();
-        getRequest();
+
 
     }
 
@@ -34,22 +34,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 System.out.println("This is a post");
 
-                EditText sleep_input = (EditText)findViewById(R.id.sleep);
-                EditText pain_input = (EditText)findViewById(R.id.pain);
-                EditText water_input = (EditText)findViewById(R.id.water);
+                EditText sleep_input = (EditText)findViewById(R.id.Sleep_Duration);
+                EditText pain_intens_input = (EditText)findViewById(R.id.Pain_Intensity);
+                EditText stress_input = (EditText)findViewById(R.id.Stress_Level);
+                EditText pain_duration_input = (EditText)findViewById(R.id.Pain_Duration);
 
                 String sleep = sleep_input.getText().toString();
-                String pain = pain_input.getText().toString();
-                String water = water_input.getText().toString();
+                String pain_intens = pain_intens_input.getText().toString();
+                String stress = stress_input.getText().toString();
+                String pain_dur = pain_duration_input.getText().toString();
 
-                System.out.println("input: " + sleep + pain + water);
+                System.out.println("input: " + sleep + pain_intens + stress + pain_dur);
 
                 // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url ="https://script.google.com/macros/s/AKfycby_J-YAi-S4DdH2WSwMOB3SjOzIqWCYqRC1fEg2ddkNJroOXiF6/exec?";
 
-                url = url + "sleep=" + sleep + "&pain=" + pain + "&water=" + water;
-
+                url += "&sleep=" + sleep + "&pain_intens=" + pain_intens + "&stress=" + stress + "&pain_dur=" + pain_dur;
                 // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>()
@@ -72,56 +71,95 @@ public class MainActivity extends AppCompatActivity {
 
 
                 // Add the request to the RequestQueue.
+                System.out.println("Final url: "+url);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
                 queue.add(stringRequest);
                 System.out.println("done POST");
-
-
             }
         });
     }
 
-    void getRequest()
-    {
-        final Button post_button = findViewById(R.id.view_id);
-        post_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("This is a GET");
-                final TextView mTextView = (TextView) findViewById(R.id.text);
-
-                // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url ="https://script.google.com/macros/s/AKfycby_J-YAi-S4DdH2WSwMOB3SjOzIqWCYqRC1fEg2ddkNJroOXiF6/exec";
-
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>()
-                        {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                System.out.println("response" + response.toString());
-                            }
-                        },
-
-                        new Response.ErrorListener()
-                        {
-                            @Override
-                            public void onErrorResponse(VolleyError error)
-                            {
-                                System.out.println("That didn't work!!!");
-                            }
-                        });
 
 
-                // Add the request to the RequestQueue.
-                queue.add(stringRequest);
-                System.out.println("done GET");
+    public void onCeckboxClicked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+        int alcohol = 0;
+        int coffee = 0;
+        int cheese = 0;
+        int chocolate = 0;
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.Alcohol:
+                if (checked)
+                    alcohol = 1;
+                url += "&alcohol=" + alcohol;
+                break;
+            case R.id.Coffee:
+                if (checked)
+                    coffee = 1;
+                url += "&coffee=" + coffee;
+                break;
+            case R.id.Chocolate:
+                System.out.println("chocoooo");
+                if (checked){
+                    chocolate = 1;
+                    System.out.println("cheched");
+                }
+                url += "&chocolate=" + chocolate;
+                System.out.println("url is" + url);
+                break;
+            case R.id.Cheese:
+                if (checked)
+                    cheese = 1;
+                url += "&cheese=" + cheese;
+                break;
+            default:
+                break;
+        }
 
-
-            }
-        });
     }
 
+//    void getRequest()
+//    {
+//        final Button post_button = findViewById(R.id.view_id);
+//        post_button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                System.out.println("This is a GET");
+//                final TextView mTextView = (TextView) findViewById(R.id.text);
+//
+//                // Instantiate the RequestQueue.
+//                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+//                String url ="https://script.google.com/macros/s/AKfycby_J-YAi-S4DdH2WSwMOB3SjOzIqWCYqRC1fEg2ddkNJroOXiF6/exec";
+//
+//                // Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>()
+//                        {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // Display the first 500 characters of the response string.
+//                                System.out.println("response" + response.toString());
+//                            }
+//                        },
+//
+//                        new Response.ErrorListener()
+//                        {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error)
+//                            {
+//                                System.out.println("That didn't work!!!");
+//                            }
+//                        });
+//
+//
+//                // Add the request to the RequestQueue.
+//                queue.add(stringRequest);
+//                System.out.println("done GET");
+//
+//
+//            }
+//        });
+//    }
 }
 
 
